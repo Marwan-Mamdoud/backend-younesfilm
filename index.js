518efc400edd32f5ed7e78e58261e840a4dfda55
Marwan <marwanmamdouh169@gmail.com>
2024-10-24 14:14:28 +0300

    firsst

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ mongoose
 
 const corsOptions = {
   origin: "https://younesfilm-frontend.vercel.app", // النطاق المسموح به
-  // origin: "https://younesfilm-frontend.vercel.app", // النطاق المسموح به
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   // allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // السماح بإرسال معلومات الاعتماد
