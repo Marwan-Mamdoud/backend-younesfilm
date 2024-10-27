a7ea3e711d2d922b8cdda158a869dfdd097ad863
Marwan <marwanmamdouh169@gmail.com>
2024-10-27 13:48:36 +0300

    test

--- index.js
+++ index.js
@@ -42,6 +42,8 @@ app.use(cors(corsOptions));
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Server is Working Seccussfully." });
+  return res
+    .status(200)
+    .json({ message: "Main Server is Working Seccussfully." });
 });
 app.use("/api", ProjectRoter);
