fb91d5e403600b1d752d548cf98d9feaf0a4d097
Marwan <marwanmamdouh169@gmail.com>
2024-10-27 14:32:53 +0300

    onther test

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(compression());
 
 const corsOptions = {
   // origin: "https://younesfilm-frontend.vercel.app", // النطاق المسموح به
-  origin: "http://localhost:3000/", // النطاق المسموح به
+  origin: "http://localhost:3000", // النطاق المسموح به
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   // allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // السماح بإرسال معلومات الاعتماد
