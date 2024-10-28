d3f4977a7cf95652fccfb13c7f619711c8d8d671
Marwan <marwanmamdouh169@gmail.com>
2024-10-28 16:08:22 +0300

    first

--- Controllers/Project.js
+++ Controllers/Project.js
@@ -216,7 +216,7 @@ export const sortedProjects = async (req, res, next) => {
 
   try {
     // Step 1: Iterate through the sorted data
-    if (sortedData?.length > 0)
+    if (sortedData && sortedData?.length > 0)
       return res.status(201).json({ message: "Allready Sorted" });
     const project = await Promise.all(
       sortedData?.map((item, index) => {
