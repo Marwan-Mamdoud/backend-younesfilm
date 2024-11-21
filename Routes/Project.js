dc31f48a6ea86524c51ae4695415e03bda51c37d
Marwan <marwanmamdouh169@gmail.com>
2024-11-21 11:22:29 +0200

    1122

--- Routes/Project.js
+++ Routes/Project.js
@@ -47,4 +47,47 @@ router.put("/update-user/:id", updateUser);
 
 router.delete("/delete-user/:id", deleteUsers);
 
+router.post("/sendMail", async (req, res, next) => {
+  try {
+    const { firstName, email, lastName, message } = req.body;
+    // console.log({ name, email, phone, rooms, state, message });
+    const transport = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "marwanmamdouh159@gmail.com",
+        pass: "casa yloo prts fdds",
+      },
+    });
+    const mailOption = {
+      from: {
+        name: `Real State`,
+        address: `${email}`,
+      },
+      // to: "ibrahimyounes646@gmail.com",
+      to: "marwanmamdouh159@gmail.com",
+      subject: "Younes Film Email",
+      text: "You just got a form submission!",
+      html: `<br/><p>You just got a form submission!</p><br/><br/>
+      <b>Form</b><br/>
+      <p>Booking From</p><br/><br/>
+      <b>Site</b><br/>
+      <p>Real State Site</p><br/><br/>
+      <b>Submitted content</b> <br/>
+      <b>First Name: ${firstName}</b><br/>
+      <b>Last Name: ${lastName}</b><br/>
+      <b>Email: ${email}</b><br/>
+      <b>Message: ${message}</b><br/>`,
+    };
+    await transport.sendMail(mailOption);
+    console.log("done");
+    return res.status(200).json({ message: "Done Send mail" });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(400).json({ error: error.message });
+  }
+});
+
 export default router;
