18e604c6c3a3d1f5751b8ce8ee306f3e584101e5
Marwan <marwanmamdouh169@gmail.com>
2024-11-24 17:51:21 +0200

    Add Link for Front-end

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(compression());
 
 const corsOptions = {
-  origin: ["https://younesfilm-frontend.vercel.app", "http://127.0.0.1:5500"], // النطاق المسموح به
+  origin: [
+    "https://younesfilm-frontend.vercel.app",
+    "http://127.0.0.1:5500",
+    "https://younes-flim.vercel.app",
+  ], // النطاق المسموح به
   // origin: "*", // النطاق المسموح به
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   // allowedHeaders: ["Content-Type", "Authorization"]
