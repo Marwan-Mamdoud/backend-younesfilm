b532f3f75768acc9771b3f449d6e4fbae1e96cc0
Marwan <marwanmamdouh169@gmail.com>
2024-12-06 14:34:20 +0200

    lang ar edit

--- Models/Category.js
+++ Models/Category.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const Schema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: Array, required: true },
 });
 
 const Categories = mongoose.model("Categories", Schema);
