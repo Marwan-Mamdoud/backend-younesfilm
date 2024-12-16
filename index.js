219d911b8ced28a3b244e6f1794da1a85217ff43
Marwan <marwanmamdouh169@gmail.com>
2024-12-16 14:26:35 +0200

    add

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const corsOptions = {
     // "http://localhost:3000",
     "https://younesfilm-frontend.vercel.app",
     "http://127.0.0.1:5500",
-    "https://younes-flim-3fnv.vercel.app/",
+    "https://younes-flim-3fnv.vercel.app",
     "https://www.younesfilm.com",
   ], // النطاق المسموح به
   // origin: "*", // النطاق المسموح به
